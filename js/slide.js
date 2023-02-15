61a2c286c44f415450bf0ac157c6ce38c966ad87
mlab <mlabpuk@gmail.com>
2023-02-15 20:12:39 +0900

    ファイル作成

---
+++ js/slide.js
@@ -0,0 +1,19 @@
+$('.photo').slick({
+  autoplay:true,
+  autoplaySpeed:3000,
+  speed:1000,
+  infinite:true,
+  slidesToShow:1,
+  slidesToScroll:1,
+  arrows:true,
+  prevArrow:'<div class="slick-prev"></div>',
+  nextArrow:'<div class="slick-next"></div>',
+  dots:true,
+  pauseOnFocus:false,
+  pauseOnHover:false,
+  pauseOnDotsHover:false,
+});
+
+$('.photo').on('touchmove',function(event,slick,currentSlide,nextSlide){
+  $('.photo').slick('slickPlay');
+});
